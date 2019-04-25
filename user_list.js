662af41715b2ef19b9abc39abcedb10500e1c86a
zahradardan <47085575+zahradardan@users.noreply.github.com>
2019-04-25 10:06:37 +0430

    final

--- user_list.js
+++ user_list.js
@@ -2,32 +2,32 @@ $(document).ready(function () {
     $.get("users.json", function (data) {
         for (var i = 0; i < data.length; i+=2) {
            // alert(data[1].userName);
-            //if(data[i].userType==="Student"){
+            if(data[i].userType!=="Student") {
                 var user_card = document.createElement("DIV");
-                user_card.className="user_card";
+                user_card.className = "user_card";
                 var user_name = document.createElement("P");
-                user_name.innerHTML=data[i].userName;
+                user_name.innerHTML = data[i].userName;
 
                 var activate = document.createElement("BUTTON");
-                activate.className="manager_act";
-                activate.innerHTML="تأیید کاربر";
-                activate.style.backgroundColor="forestgreen";
+                activate.className = "manager_act";
+                activate.innerHTML = "تأیید کاربر";
+                activate.style.backgroundColor = "forestgreen";
 
                 var unActive = document.createElement("BUTTON");
-                unActive.className="manager_act";
-                unActive.innerHTML="غیر فعال کردن کاربر";
-                unActive.style.backgroundColor="gold";
+                unActive.className = "manager_act";
+                unActive.innerHTML = "غیر فعال کردن کاربر";
+                unActive.style.backgroundColor = "gold";
 
                 var delet_user = document.createElement("BUTTON");
-                delet_user.className="manager_act";
-                delet_user.innerHTML="حذف کاربر";
-                delet_user.style.backgroundColor="red";
+                delet_user.className = "manager_act";
+                delet_user.innerHTML = "حذف کاربر";
+                delet_user.style.backgroundColor = "red";
 
 
                 var details = document.createElement("BUTTON");
-                details.className="manager_act";
-                details.innerHTML="مشاهده جزئیات پروفایل";
-                details.style.backgroundColor="blue";
+                details.className = "manager_act";
+                details.innerHTML = "مشاهده جزئیات پروفایل";
+                details.style.backgroundColor = "blue";
 
 
                 var line = document.createElement("BR");
@@ -39,10 +39,11 @@ $(document).ready(function () {
                 user_card.appendChild(details);
                 document.getElementById("r_part").appendChild(user_card);
             }
+            }
       //  }
         for (var i = 1; i < data.length; i+=2) {
             // alert(data[1].userName);
-            if(data[i].userType==="Student"){
+            if(data[i].userType!=="Student"){
                 var user_card = document.createElement("DIV");
                 user_card.className="user_card";
                 var user_name = document.createElement("P");
