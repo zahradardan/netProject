01df61aabc4e7f75f958ac1972fac03cb79308e1
zahradardan <47085575+zahradardan@users.noreply.github.com>
2019-04-25 11:11:50 +0430

    Add files via upload

--- report.js
+++ report.js
@@ -134,6 +134,20 @@ function date_base() {
     var date = document.getElementById("date");
     var string = date.value;
 
-
-
 }
+$(document).ready(function () {
+    $("#transform").click(function () {
+        var links=document.getElementsByClassName("link");
+        var visibile=document.getElementById("topnav").getAttribute("visible");
+        if(visibile==="false"){
+            document.getElementById("topnav").setAttribute("visible","true");
+            for(var i=0;i<links.length;i++)
+                links[i].style.display='inline-block';
+        }
+        if(visibile==="true"){
+            document.getElementById("topnav").setAttribute("visible","false");
+            for(var i=0;i<links.length;i++)
+                links[i].style.display='none';
+        }
+    });
+})
