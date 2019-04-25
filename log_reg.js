7e83fe8e92ebe474583cda2145f3c5b5bc01991f
zahradardan <47085575+zahradardan@users.noreply.github.com>
2019-04-25 10:07:50 +0430

    final

--- log_reg.js
+++ log_reg.js
@@ -1,12 +1,4 @@
-$(document).ready(function () {
-    $.get("users.json", function (data) {
-        for (var i = 0; i < data.length; i++) {
-            alert(data[1].userName);
-        }
-    });
-
-});
-    function open_modal() {
+ function open_modal() {
         var modal = document.getElementById("Login_modal");
         modal.style.display = "block";
         modal.style.transition = "2s";
@@ -18,3 +10,30 @@ $(document).ready(function () {
         modal.style.display = "none";
     }
 
+ function not_empty() {
+    var go = true;
+     var string2 = document.getElementById("pass").value;
+     var string1 = document.getElementById("user_name").value;
+
+     var error1 = document.getElementById("warn1");
+     var error2 = document.getElementById("warn2");
+
+     if(string1.length === 0) {
+         error1.style.visibility = "visible";
+         go=false;
+     }else{
+         error1.style.visibility = "hidden";
+     }
+
+     if(string2.length === 0) {
+         error2.style.visibility = "visible";
+         go = false;
+     }else{
+         error2.style.visibility = "hidden";
+     }
+     if(go === true){
+
+         window.location.href = "editprofile.html";
+
+     }
+ }
